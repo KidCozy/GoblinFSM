c499cff26153075eb2e349bd2f430235a4b2f976
C0zyB0yz <pencube1214@naver.com>
2018-11-26 23:54:18 +0900

    aa
    Init

---
+++ Assets/Scripts/StatData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatData : ScriptableObject
+{
+    public float maxHp = 100.0f;
+    public float baseRange = 50.0f;
+
+    public float goblinHp = 500.0f;
+    public float goblinRange = 70.0f;
+}
